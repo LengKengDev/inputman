6dbb71b264cb6e20878b54ef068abd2cda484d0d
ThienLV <le.vinh.thien@framgia.com>
2018-12-09 00:37:37 +0900

    add layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,14 +21,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-new Vue({
-  el: '#app'
-});
